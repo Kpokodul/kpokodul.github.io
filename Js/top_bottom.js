b199ce3dc7370a50af9c0525bf94114018d64a17
Kpokodul <51588090+Kpokodul@users.noreply.github.com>
2019-07-22 14:44:52 +0300

    top_bottom

---
+++ Js/top_bottom.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 60) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
+});
