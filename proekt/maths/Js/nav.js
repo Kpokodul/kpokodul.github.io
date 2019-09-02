2167ac2d6f962733c520e6ea378be301a2a256d0
Kpokodul <51588090+Kpokodul@users.noreply.github.com>
2019-09-02 20:44:59 +0300

    proekt (maths)

---
+++ proekt/maths/Js/nav.js
@@ -0,0 +1,11 @@
+$(Document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });//end slide toggle
+  
+  $(window).resize(function() {		
+		if (  $(window).width() > 500 ) {			
+			$('nav ul').removeAttr('style');
+		 }
+	});
+});
